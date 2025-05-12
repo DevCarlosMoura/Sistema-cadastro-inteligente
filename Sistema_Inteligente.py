a281ed5f86b6b4d6ebe787e7a3f0a8871dbbd033
Carlos Eduardo Moura Pinheiro <dev.carlosmoura@gmail.com>
2025-05-12 16:37:34 -0300

    Add Commit Intelligent system project

--- Sistema_Inteligente.py
+++ Sistema_Inteligente.py
@@ -12,12 +12,12 @@ while True:
     
     option = input('Choose an option: ')
     
-    if option == '1':
+    if option == '1': #Variaveis da 'option 1' criadas para cadastro de nova pessoa.
         person_name = input('Write your name: ')
         person_age = int(input('Write your age: '))
         person_profession = input('Write your profession: ')
         
-        person = {
+        person = {      #Criando um dicionario person com os dados, esse dicionario vai ser adicionado na lista registrations.
             'name': person_name,
             'age': person_age,
             'profession': person_profession
@@ -25,7 +25,7 @@ while True:
         
         registrations.append(person)
         print(f'{person_name} has been succesfully registered!')
-        print(registrations)
+        print(registrations) #Isso mostra a lista completa e verifica o que foi armazenado.
         
     elif option == '2':
         if not registrations:
@@ -48,22 +48,22 @@ while True:
         if not registrations:
             print('No registrations found. ')
         else:
-            profession_count = {}
+            profession_count = {} #Criando um dicionario para contar as profissoes.
             for person in registrations:
                 profession = person['profession']
                 if profession in profession_count:
                     profession_count[profession] += 1
                 else:
                     profession_count[profession] = 1    
-            sorted_professions = sorted(profession_count.items(), key = lambda x:x[1], reverse=True)
+            sorted_professions = sorted(profession_count.items(), key = lambda x:x[1], reverse=True) #Ordenando o dicionario por valor(quantidade de pessoas) de forma decrescente.
             print('\n--- The most commom profession ---')
             for profession, count in sorted_professions:
                 print(f'Profession: {profession}, Count: {count}')
                 
     elif option == '5':
         print('Exiting the system. See you later!')
-        break
+        break #Encerra o laco while e finaliza o programa.
 
     else:
-        print('Invalid option. Please try again.')
+        print('Invalid option. Please try again.') #Trata entradas invalidas.
 
